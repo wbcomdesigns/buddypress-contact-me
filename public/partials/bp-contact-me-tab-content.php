8a066fae446b9ee09434e88a453716122eb2dfed
Wbcom Dev <pallavi@wbcomdesigns.com>
2023-01-20 19:30:22 +0530

    save contact me form data

--- public/partials/bp-contact-me-tab-content.php
+++ public/partials/bp-contact-me-tab-content.php
@@ -16,16 +16,26 @@ $max  = 300;
 $num1 = rand( $min, $max );
 $num2 = rand( $min, $max );
 $sum  = $num1 + $num2;
+if( isset( $_POST['bp_contact_me_form_save'] ) ){
+    $bp_display_user_id = bp_displayed_user_id();
+    $bp_contact_me_subject = $_POST['bp_contact_me_subject'];
+    $bp_contact_me_msg = $_POST['bp_contact_me_msg'];
+    update_user_meta( $bp_display_user_id, 'contact_me_user_sub', $bp_contact_me_subject );
+    update_user_meta( $bp_display_user_id, 'contact_me_user_msg', $bp_contact_me_msg );
+}
+$bp_display_user_id = bp_displayed_user_id();
+$contact_me_user_sub = get_user_meta( $bp_display_user_id, 'contact_me_user_sub' );
+$contact_me_user_msg = get_user_meta( $bp_display_user_id, 'contact_me_user_msg' );
 ?>
 <div class="bp-content-me-container">
     <h3><?php esc_html_e("Contact Me Form", 'bp-contact-me'); ?></h3>
     <div class="bp-member-blog-post-form">
         <form id="bp-member-post" class="bp-contact-me-form" method="post" action="" enctype="multipart/form-data" >
             <label for="bp_contact_me_subject"><?php esc_html_e('Subject:', 'bp-contact-me'); ?>
-                <input type="text" name="bp_contact_me_subject" value="" required/>
+                <input type="text" name="bp_contact_me_subject" value="<?php echo isset( $contact_me_user_sub[0] ) ? $contact_me_user_sub[0] : '';?>" required/>
             </label>
             <label for="bp_contact_me_message"><?php esc_html_e('Message:', 'bp-contact-me'); ?>
-                <textarea name="bp_contact_me_msg" rows="10" cols="100" required></textarea>
+                <textarea name="bp_contact_me_msg" rows="10" cols="100" required><?php echo isset( $contact_me_user_msg[0] ) ? $contact_me_user_msg[0] : '';?></textarea>
             </label>
             <label for="captchasum" class="captchasum">
                 <?php echo $num1 . '+' . $num2; ?>?
