fc1c9d31d45bfc48680b9df0181365d93ee66326
Wbcom Dev <pallavi@wbcomdesigns.com>
2023-02-10 17:44:04 +0530

    Added faq

--- admin/inc/buddypress-contact-me-support-tab.php
+++ admin/inc/buddypress-contact-me-support-tab.php
@@ -18,11 +18,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<div class="wbcom-faq-admin-row">
 				<div class="wbcom-faq-section-row">
 					<button class="blpro-accordion wbcom-faq-accordion">
-						<?php esc_html_e( 'Does This plugin requires BuddyPress?', 'buddypress-contact-me' ); ?>
+						<?php esc_html_e( 'Does This Plugin supports BB Platform?', 'buddypress-contact-me' ); ?>
 					</button>
 					<div class="wbcom-faq-panel">
 						<p>
-							<?php esc_html_e( 'Yes, It needs you to have BuddyPress installed and activated.', 'buddypress-contact-me' ); ?>
+							<?php esc_html_e( 'Yes! BuddyPress Contact Me plugin works with BB Platform.', 'buddypress-contact-me' ); ?>
 						</p>
 					</div>
 				</div>
@@ -30,13 +30,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<div class="wbcom-faq-admin-row">
 				<div class="wbcom-faq-section-row">
 					<button class="blpro-accordion wbcom-faq-accordion">
-						<?php esc_html_e( 'What are the settings for non logged-in users ?', 'buddypress-contact-me' ); ?>
+						<?php esc_html_e( 'How can we show contact me form on any post or page?', 'buddypress-contact-me' ); ?>
 					</button>
 					<div class="wbcom-faq-panel">
 						<p>
-							<?php esc_html_e( 'This plugin allows the site administrator to secure components of BuddyPress (if active), WordPress pages, custom post types from non-logged in users.', 'buddypress-contact-me' ); ?>
+							<?php esc_html_e( 'You can use [bp-contact-me id="USER_ID"] to show user specific contact form on any post or page.  replace id of the user with the parameter `USER_ID` .', 'buddypress-contact-me' ); ?>
 						</p>
-						<p><?php _e( 'You can lockdown WordPress Pages, any Custom Post Type and any BuddyPress Component and can have some content displayed like if you want to show any shortcode content or any simple message.', 'buddypress-contact-me' );?></p>
+					</div>
+				</div>
+			</div>
+			<div class="wbcom-faq-admin-row">
+				<div class="wbcom-faq-section-row">
+					<button class="blpro-accordion wbcom-faq-accordion">
+						<?php esc_html_e( 'Does this plugin works in logged out mode?', 'buddypress-contact-me' ); ?>
+					</button>
+					<div class="wbcom-faq-panel">
+						<p>
+							<?php esc_html_e( 'Yes! Logged out users(Visitors) can also contect with the site users.', 'buddypress-contact-me' ); ?>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="wbcom-faq-admin-row">
+				<div class="wbcom-faq-section-row">
+					<button class="blpro-accordion wbcom-faq-accordion">
+						<?php esc_html_e( 'What are the requirements for this plugin to work?', 'buddypress-contact-me' ); ?>
+					</button>
+					<div class="wbcom-faq-panel">
+						<p>
+							<?php esc_html_e( 'BuddyPress should be installed and activated on your site for this plugin to work.', 'buddypress-contact-me' ); ?>
+						</p>
 					</div>
 				</div>
 			</div>
