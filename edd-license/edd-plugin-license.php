371a67516b87905ade3e1754b3936558626e6d94
Dishant Patel <dishant.patel1007@gmail.com>
2023-05-02 18:08:49 +0530

    Update License activation file and set response in transient

--- edd-license/edd-plugin-license.php
+++ edd-license/edd-plugin-license.php
@@ -308,6 +308,7 @@ function edd_wbcom_BCM_admin_notices() {
 			case 'expired':
 				?>
 				<div class="notice notice-error is-dismissible">
+				<p>
 				<?php 
 				echo $message = sprintf(
 							/* translators: %1$s: Expire Time*/
@@ -315,6 +316,7 @@ function edd_wbcom_BCM_admin_notices() {
 							date_i18n( get_option( 'date_format' ), strtotime( $license_data->expires, current_time( 'timestamp' ) ) )
 						);
 				?>
+				</p>
 				</div>
 				<?php
 					
