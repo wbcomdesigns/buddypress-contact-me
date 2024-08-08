416712a579f0f8351f14dfbf3ded794ba63be556
Varun Kumar Dubey <vapvarun@gmail.com>
2024-08-09 03:00:46 +0530

    updating language

--- public/partials/bp-contact-me-tab-content.php
+++ public/partials/bp-contact-me-tab-content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Provide a public-facing view for the plugin
  *
@@ -10,69 +11,69 @@
  * @package    Buddypress_Contact_Me
  * @subpackage Buddypress_Contact_Me/public/partials
  */
+
 $min  = 1;
 $max  = 20;
-$num1 = rand( $min, $max );
-$num2 = rand( $min, $max );
+$num1 = rand($min, $max);
+$num2 = rand($min, $max);
 $sum  = $num1 + $num2;
 ?>
 <div class="bp-content-me-container">
 	<div class="bp-member-blog-post-form">
-		<form id="bp-member-post" class="bp-contact-me-form" method="post" action="" enctype="multipart/form-data" >
-			<?php if ( ! is_user_logged_in() ) { ?>
-				<div class="bp-content-me-fieldset" for="bp_contact_me_first_name">
-					<label>
-						<?php esc_html_e( 'Name', 'buddypress-contact-me' ); ?>
-						<span><?php esc_html_e( '*', 'buddypress-contact-me' ); ?></span>
+		<form id="bp-member-post" class="bp-contact-me-form" method="post" action="" enctype="multipart/form-data">
+			<?php if (!is_user_logged_in()) : ?>
+				<div class="bp-content-me-fieldset">
+					<label for="bp_contact_me_first_name">
+						<?php esc_html_e('Name', 'buddypress-contact-me'); ?>
+						<span><?php esc_html_e('*', 'buddypress-contact-me'); ?></span>
 					</label>
-					<input type="text" class="bp_contact_me_first_name" name="bp_contact_me_first_name" required/>
+					<input type="text" class="bp_contact_me_first_name" name="bp_contact_me_first_name" required />
 				</div>
-				<div class="bp-content-me-fieldset" for="bp_contact_me_email">
-					<label>
-						<?php esc_html_e( 'Email', 'buddypress-contact-me' ); ?>
-						<span><?php esc_html_e( '*', 'buddypress-contact-me' ); ?></span>
+				<div class="bp-content-me-fieldset">
+					<label for="bp_contact_me_email">
+						<?php esc_html_e('Email', 'buddypress-contact-me'); ?>
+						<span><?php esc_html_e('*', 'buddypress-contact-me'); ?></span>
 					</label>
-					<input type="email" class="bp_contact_me_email" name="bp_contact_me_email" required/>
+					<input type="email" class="bp_contact_me_email" name="bp_contact_me_email" required />
 				</div>
-				<?php
-			}
-			if ( is_user_logged_in() ) {
-				$name = bp_core_get_user_displayname( bp_loggedin_user_id() );
-				?>
-			<div class="bp-content-me-fieldset" for="bp_contact_me_login_name">
-				<label>
-					<?php esc_html_e( 'Name', 'buddypress-contact-me' ); ?>
-					<span><?php esc_html_e( '*', 'buddypress-contact-me' ); ?></span>
-			</label>
-				<input type="text" name="bp_contact_me_login_name" class="bp_contact_me_login_name" value="<?php echo esc_attr( $name ); ?>" required/>
+			<?php else :
+				$name = bp_core_get_user_displayname(bp_loggedin_user_id());
+			?>
+				<div class="bp-content-me-fieldset">
+					<label for="bp_contact_me_login_name">
+						<?php esc_html_e('Name', 'buddypress-contact-me'); ?>
+						<span><?php esc_html_e('*', 'buddypress-contact-me'); ?></span>
+					</label>
+					<input type="text" name="bp_contact_me_login_name" class="bp_contact_me_login_name" value="<?php echo esc_attr($name); ?>" required />
+				</div>
+			<?php endif; ?>
+
+			<div class="bp-content-me-fieldset">
+				<label for="bp_contact_me_subject">
+					<?php esc_html_e('Subject', 'buddypress-contact-me'); ?>
+					<span><?php esc_html_e('*', 'buddypress-contact-me'); ?></span>
+				</label>
+				<input type="text" class="bp_contact_me_subject" name="bp_contact_me_subject" required />
 			</div>
-			<?php } ?>	
-			<div class="bp-content-me-fieldset" for="bp_contact_me_subject">
-				<label>
-					<?php esc_html_e( 'Subject', 'buddypress-contact-me' ); ?>
-					<span><?php esc_html_e( '*', 'buddypress-contact-me' ); ?></span>
-			</label>
-				<input type="text" class="bp_contact_me_subject" name="bp_contact_me_subject" required/>
-			</div>			
-			<div class="bp-content-me-fieldset" for="bp_contact_me_message">
-				<label>
-					<?php esc_html_e( 'Message', 'buddypress-contact-me' ); ?>
-					<span><?php esc_html_e( '*', 'buddypress-contact-me' ); ?></span>
-					</label>
+			<div class="bp-content-me-fieldset">
+				<label for="bp_contact_me_message">
+					<?php esc_html_e('Message', 'buddypress-contact-me'); ?>
+					<span><?php esc_html_e('*', 'buddypress-contact-me'); ?></span>
+				</label>
 				<textarea name="bp_contact_me_msg" class="bp_contact_me_msg" rows="10" cols="100" required></textarea>
 			</div>
 			<div id="fieldset-captchasum" class="bp-content-me-fieldset">
-				<div for="captchasum" class="captchasum">
-					<?php echo esc_attr( $num1 . '+' . $num2 ); ?>?
+				<div class="captchasum">
+					<?php echo esc_html("$num1 + $num2"); ?>?
 				</div>
-				<input type="hidden" name="bcm_shortcode_user_id" value="<?php echo isset( $atts['id'] ) ? esc_html( $atts['id'] ) : ''; ?>"/>			
-				<input type="hidden" name="bcm_shortcode_username" value="<?php echo isset( $atts['user'] ) ? esc_html( $atts['user'] ) : ''; ?>"/>			
-				<input type="hidden" name="bcm_nonce" value="<?php echo esc_attr( wp_create_nonce( 'bcm_form_nonce' ) ); ?>">
+				<input type="hidden" name="bcm_shortcode_user_id" value="<?php echo isset($atts['id']) ? esc_attr($atts['id']) : ''; ?>" />
+				<input type="hidden" name="bcm_shortcode_username" value="<?php echo isset($atts['user']) ? esc_attr($atts['user']) : ''; ?>" />
+				<input type="hidden" name="bcm_nonce" value="<?php echo esc_attr(wp_create_nonce('bcm_form_nonce')); ?>">
 				<div class="bp_contact_me_captcha_text">
 					<input type="text" class="form-control captcha-control" id="captcha-val" required>
 				</div>
 			</div>
-				<input data-captcha="<?php echo esc_attr( $sum ); ?>" name="bp_contact_me_form_save" class="bp-contact-me-btn" type="submit" value="<?php echo esc_attr__( 'Submit', 'buddypress-contact-me' ); ?>"/>
+			<input data-captcha="<?php echo esc_attr($sum); ?>" name="bp_contact_me_form_save" class="bp-contact-me-btn" type="submit" value="<?php esc_attr_e('Submit', 'buddypress-contact-me'); ?>" />
 		</form>
 	</div>
 </div>
--- public/partials/bp-contact-tab-show-sender-user-data.php
+++ public/partials/bp-contact-tab-show-sender-user-data.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * Provide a public-facing view for the plugin
+ * Provide a public-facing view for the plugin.
  *
  * This file is used to markup the public-facing aspects of the plugin.
  *
@@ -10,111 +11,98 @@
  * @package    Buddypress_Contact_Me
  * @subpackage Buddypress_Contact_Me/public/partials
  */
+
 global $wpdb;
-$loggedin_user_id         = get_current_user_id();
+$loggedin_user_id = get_current_user_id();
 $bp_contact_me_table_name = $wpdb->prefix . 'contact_me';
-$get_contact_row          = "SELECT * FROM $bp_contact_me_table_name  WHERE `reciever` = $loggedin_user_id ORDER BY `id` DESC";
-$get_contact_allrow       = $wpdb->get_results( $get_contact_row, ARRAY_A );
+$get_contact_row = $wpdb->prepare(
+	"SELECT * FROM $bp_contact_me_table_name WHERE `reciever` = %d ORDER BY `id` DESC",
+	$loggedin_user_id
+);
+$get_contact_allrow = $wpdb->get_results($get_contact_row, ARRAY_A);
 ?>
-<div class="bp-contact-me-detials">
-<?php if ( $get_contact_allrow ) { ?>
-	<div class="bp-contact-me-loader" style="display:none;">
-		<div class="bp-contact-me-loader-img">
-			<img src="<?php echo esc_url( BUDDYPRESS_CONTACT_ME_PLUGIN_URL . '/public/images/loader.gif' ); ?>"/>
+<div class="bp-contact-me-details">
+	<?php if ($get_contact_allrow) : ?>
+		<div class="bp-contact-me-loader" style="display:none;">
+			<div class="bp-contact-me-loader-img">
+				<img src="<?php echo esc_url(BUDDYPRESS_CONTACT_ME_PLUGIN_URL . 'public/images/loader.gif'); ?>" alt="<?php esc_attr_e('Loading...', 'buddypress-contact-me'); ?>" />
+			</div>
 		</div>
-	</div>
-	<form method="post">		
-	<table class="bp_contact-me-messages">
-			<thead>
-				<tr>
-					<th class="contact-me-sender-id"><input type="checkbox" id="bcm-select-all-contact"/></th>                
-					<th class="contact-me-subject"><?php esc_html_e( 'Name', 'buddypress-contact-me' ); ?></th>
-					<th class="contact-me-message"><?php esc_html_e( 'Message', 'buddypress-contact-me' ); ?></th>
-					<th class="contact-me-btn"><?php esc_html_e( 'Action', 'buddypress-contact-me' ); ?></th>            
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-				foreach ( $get_contact_allrow as $get_contact_allrow_val ) {
-					$sender_id = $get_contact_allrow_val['sender'];
-					$subject   = $get_contact_allrow_val['subject'];
-					$message   = $get_contact_allrow_val['message'];
-					if ( 0 != $sender_id ) {
-						$bcm_first_name = bp_core_get_user_displayname( $sender_id );
-					} else {
-						$bcm_first_name = $get_contact_allrow_val['name'];
-					}
-					?>
-				<tr>
-					<td class="contact-me-sender-id"><input type="checkbox" name="bcm_messages[]" class="bcm-all-check" value="<?php echo esc_attr( $get_contact_allrow_val['id'] ); ?>"/></td>
-					<td class="user_displayname">
-						<?php if ( function_exists( 'buddypress' ) && version_compare( buddypress()->version, '12.0', '>=' ) ) { ?>
-						<a href="<?php echo esc_attr( bp_members_get_user_url( $sender_id ) ); ?>" title="<?php echo esc_attr( bp_core_get_user_displayname( $sender_id ) ); ?>">
-					 <?php } else { ?>
-						<a href="<?php echo esc_attr( bp_core_get_user_domain( $sender_id ) ); ?>" title="<?php echo esc_attr( bp_core_get_user_displayname( $sender_id ) ); ?>">
-						<?php } ?>
-					<?php
-						echo wp_kses_post(
-							bp_core_fetch_avatar(
-								array(
-									'item_id' => $sender_id,
-									'type'    => 'full',
-								)
-							)
-						);
-						echo esc_html( $bcm_first_name );
-					?>
-						</a>
-					</td>
-					<td>
-						<div class="bcm-user-subject">
-							<?php echo esc_html( $subject ); ?>
-						</div>
-						<div class="bcm-user-message">
-							<?php echo esc_html( $message ); ?>
-						</div>
-					</td>
-					<td>		
-						<div class="bcm_action">
-							<div class="bcm_action_btn">
-								<span class="dashicons dashicons-visibility bcm_message_seen" data-id="<?php echo esc_attr( $get_contact_allrow_val['id'] ); ?>" aria-hidden="true"></span>
-								<small class="bcm-tooltip-text"><?php esc_html_e( 'View Message', 'buddypress-contact-me' ); ?></small>
-							</div>
-							<button class="bcm_action_btn" id="bcm_message_delete">
-								<span class="dashicons dashicons-dismiss bcm_message_delete" data-id="<?php echo esc_attr( $get_contact_allrow_val['id'] ); ?>" aria-hidden="true"></span>
-								<small class="bcm-tooltip-text"><?php esc_html_e( 'Delete Message', 'buddypress-contact-me' ); ?></small>
-							</button>
-						</div>
-					</td>
-				</tr>
-					<?php
-				}
-				?>
-			</tbody>
-	</table>
-	<div class="bcm-contact-options-nav">
-		<div class="select-wrap">
-			<label class="bp-screen-reader-text" for="bcm-select">
-				<?php esc_html_e( 'Select Bulk Action', 'buddypress-contact-me' ); ?>
-			</label>
-			<select name="bcm_contact_bulk_action" id="bcm-select">
-				<option value="" selected="selected"><?php esc_html_e( 'Bulk Actions', 'buddypress-contact-me' ); ?></option>
-				<option value="delete"><?php esc_html_e( 'Delete', 'buddypress-contact-me' ); ?></option>
-			</select>
+		<form method="post">
+			<table class="bp_contact-me-messages">
+				<thead>
+					<tr>
+						<th class="contact-me-sender-id"><input type="checkbox" id="bcm-select-all-contact" /></th>
+						<th class="contact-me-subject"><?php esc_html_e('Name', 'buddypress-contact-me'); ?></th>
+						<th class="contact-me-message"><?php esc_html_e('Message', 'buddypress-contact-me'); ?></th>
+						<th class="contact-me-btn"><?php esc_html_e('Action', 'buddypress-contact-me'); ?></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach ($get_contact_allrow as $get_contact_allrow_val) : ?>
+						<?php
+						$sender_id = $get_contact_allrow_val['sender'];
+						$subject = $get_contact_allrow_val['subject'];
+						$message = $get_contact_allrow_val['message'];
+						$bcm_first_name = $sender_id ? bp_core_get_user_displayname($sender_id) : $get_contact_allrow_val['name'];
+						?>
+						<tr>
+							<td class="contact-me-sender-id"><input type="checkbox" name="bcm_messages[]" class="bcm-all-check" value="<?php echo esc_attr($get_contact_allrow_val['id']); ?>" /></td>
+							<td class="user_displayname">
+								<a href="<?php echo esc_url(bp_core_get_user_domain($sender_id)); ?>" title="<?php echo esc_attr(bp_core_get_user_displayname($sender_id)); ?>">
+									<?php
+									echo wp_kses_post(
+										bp_core_fetch_avatar(
+											array(
+												'item_id' => $sender_id,
+												'type'    => 'full',
+											)
+										)
+									);
+									echo esc_html($bcm_first_name);
+									?>
+								</a>
+							</td>
+							<td>
+								<div class="bcm-user-subject"><?php echo esc_html($subject); ?></div>
+								<div class="bcm-user-message"><?php echo esc_html($message); ?></div>
+							</td>
+							<td>
+								<div class="bcm_action">
+									<div class="bcm_action_btn">
+										<span class="dashicons dashicons-visibility bcm_message_seen" data-id="<?php echo esc_attr($get_contact_allrow_val['id']); ?>" aria-hidden="true"></span>
+										<small class="bcm-tooltip-text"><?php esc_html_e('View Message', 'buddypress-contact-me'); ?></small>
+									</div>
+									<button class="bcm_action_btn" id="bcm_message_delete" type="button">
+										<span class="dashicons dashicons-dismiss bcm_message_delete" data-id="<?php echo esc_attr($get_contact_allrow_val['id']); ?>" aria-hidden="true"></span>
+										<small class="bcm-tooltip-text"><?php esc_html_e('Delete Message', 'buddypress-contact-me'); ?></small>
+									</button>
+								</div>
+							</td>
+						</tr>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
+			<div class="bcm-contact-options-nav">
+				<div class="select-wrap">
+					<label class="bp-screen-reader-text" for="bcm-select">
+						<?php esc_html_e('Select Bulk Action', 'buddypress-contact-me'); ?>
+					</label>
+					<select name="bcm_contact_bulk_action" id="bcm-select">
+						<option value="" selected="selected"><?php esc_html_e('Bulk Actions', 'buddypress-contact-me'); ?></option>
+						<option value="delete"><?php esc_html_e('Delete', 'buddypress-contact-me'); ?></option>
+					</select>
+				</div>
+				<input type="submit" id="bcm-bulk-manage" class="button action" value="<?php esc_attr_e('Apply', 'buddypress-contact-me'); ?>">
+			</div>
+			<?php wp_nonce_field('bcm_contact_bulk_nonce', 'bcm_contact_bulk_nonce'); ?>
+		</form>
+	<?php else : ?>
+		<div class="bp-contact-me-container contact-me-not-found">
+			<div id="message" class="info bp-feedback bp-messages bp-template-notice">
+				<span class="bp-icon" aria-hidden="true"></span>
+				<p><?php esc_html_e('Unfortunately, there are no contact messages found.', 'buddypress-contact-me'); ?></p>
+			</div>
 		</div>
-		<input type="submit" id="bcm-bulk-manage" class="button action" value="<?php esc_attr_e( 'Apply', 'buddypress-contact-me' ); ?>">
-	</div>	
-	<?php wp_nonce_field( 'bcm_contact_bulk_nonce', 'bcm_contact_bulk_nonce' ); ?>
-	</form>
+	<?php endif; ?>
 </div>
-	<?php
-} else {
-	?>
-	<div class="bp-contact-me-container contact-me-not-found">
-	<div id="message" class="info bp-feedback bp-messages bp-template-notice">
-		<span class="bp-icon" aria-hidden="true"></span>
-		<p><?php echo esc_html( 'Unfortunately, there are no contact messages found.' ); ?></p>
-	</div>
-	</div>
-	</div>
-	<?php } ?>
