bbecfda070ec107fb68267485351603e470b9366
Varun Kumar Dubey <vapvarun@gmail.com>
2024-08-09 02:37:46 +0530

    Improved Help & Support Resources content

--- admin/inc/buddypress-contact-me-welcome-page.php
+++ admin/inc/buddypress-contact-me-welcome-page.php
@@ -1,13 +1,14 @@
 <?php
+
 /**
  * This file is used for rendering and saving plugin welcome settings.
  *
  * @since    1.0.0
- * @author   Wbcom Designs
  * @package  Bp_Add_Group_Types
+ * @author   Wbcom Designs
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 ?>
@@ -15,42 +16,45 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="wbcom-tab-content">
 	<div class="wbcom-welcome-main-wrapper">
 		<div class="wbcom-welcome-head">
-				<p class="wbcom-welcome-description">
-				<?php esc_html_e( 'BuddyPress Contact Me plugin comes with the feature that members can contact to each other without friendship.', 'buddypress-contact-me' ); ?>
-				</p>
+			<p class="wbcom-welcome-description">
+				<?php esc_html_e('The BuddyPress Contact Me plugin allows members to contact each other without needing to be friends.', 'buddypress-contact-me'); ?>
+			</p>
 		</div><!-- .wbcom-welcome-head -->
 
 		<div class="wbcom-welcome-content">
 			<div class="wbcom-welcome-support-info">
-				<h3><?php esc_html_e( 'Help &amp; Support Resources', 'buddypress-contact-me' ); ?></h3>
-				<p><?php esc_html_e( 'Here are all the resources you may need to get help from us. Documentation is usually the best place to start. Should you require help anytime, our customer care team is available to assist you at the support center.', 'buddypress-contact-me' ); ?></p>
+				<h3><?php esc_html_e('Help & Support Resources', 'buddypress-contact-me'); ?></h3>
+				<p><?php esc_html_e('Here are the resources you may need to get help. Documentation is usually the best place to start. Should you need assistance, our customer care team is available at the support center.', 'buddypress-contact-me'); ?></p>
 
 				<div class="wbcom-support-info-wrap">
+					<!-- Documentation Widget -->
 					<div class="wbcom-support-info-widgets">
 						<div class="wbcom-support-inner">
-							<h3><span class="dashicons dashicons-book"></span><?php esc_html_e( 'Documentation', 'buddypress-contact-me' ); ?></h3>
-							<p><?php esc_html_e( 'We have prepared an extensive guide on BuddyPress Sticky Post to learn all aspects of the plugin. You will find most of your answers here.', 'buddypress-contact-me' ); ?></p>
-							<a href="<?php echo esc_url( 'https://docs.wbcomdesigns.com/doc_category/buddypress-contact-me/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Read Documentation', 'buddypress-contact-me' ); ?></a>
+							<h3><span class="dashicons dashicons-book"></span><?php esc_html_e('Documentation', 'buddypress-contact-me'); ?></h3>
+							<p><?php esc_html_e('We have prepared an extensive guide on BuddyPress Sticky Post to cover all aspects of the plugin. Most of your questions will be answered here.', 'buddypress-contact-me'); ?></p>
+							<a href="<?php echo esc_url('https://docs.wbcomdesigns.com/doc_category/buddypress-contact-me/'); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e('Read Documentation', 'buddypress-contact-me'); ?></a>
 						</div>
 					</div>
 
+					<!-- Support Center Widget -->
 					<div class="wbcom-support-info-widgets">
 						<div class="wbcom-support-inner">
-							<h3><span class="dashicons dashicons-sos"></span><?php esc_html_e( 'Support Center', 'buddypress-contact-me' ); ?></h3>
-							<p><?php esc_html_e( 'We strive to offer the best customer care via our support center. Once your theme is activated, you can ask us for help anytime.', 'buddypress-contact-me' ); ?></p>
-							<a href="<?php echo esc_url( 'https://wbcomdesigns.com/support/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Get Support', 'buddypress-contact-me' ); ?></a>
+							<h3><span class="dashicons dashicons-sos"></span><?php esc_html_e('Support Center', 'buddypress-contact-me'); ?></h3>
+							<p><?php esc_html_e('We strive to offer the best customer care via our support center. Once your theme is activated, you can request help anytime.', 'buddypress-contact-me'); ?></p>
+							<a href="<?php echo esc_url('https://wbcomdesigns.com/support/'); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e('Get Support', 'buddypress-contact-me'); ?></a>
 						</div>
 					</div>
-					<div class="wbcom-support-info-widgets">
-						<div class="wbcom-support-inner">
-							<h3><span class="dashicons dashicons-admin-comments"></span><?php esc_html_e( 'Got Feedback?', 'buddypress-contact-me' ); ?></h3>
-							<p><?php esc_html_e( 'We want to hear about your experience with the plugin. We would also love to hear any suggestions you may for future updates.', 'buddypress-contact-me' ); ?></p>
-							<a href="<?php echo esc_url( 'https://wbcomdesigns.com/contact/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Send Feedback', 'buddypress-contact-me' ); ?></a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
 
-	</div><!-- .wbcom-welcome-content -->
-</div><!-- .wbcom-welcome-main-wrapper -->
+					<!-- Feedback Widget -->
+					<div class="wbcom-support-info-widgets">
+						<div class="wbcom-support-inner">
+							<h3><span class="dashicons dashicons-admin-comments"></span><?php esc_html_e('Got Feedback?', 'buddypress-contact-me'); ?></h3>
+							<p><?php esc_html_e('We want to hear about your experience with the plugin. We would also love to hear any suggestions you may have for future updates.', 'buddypress-contact-me'); ?></p>
+							<a href="<?php echo esc_url('https://wbcomdesigns.com/contact/'); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e('Send Feedback', 'buddypress-contact-me'); ?></a>
+						</div>
+					</div>
+				</div><!-- .wbcom-support-info-wrap -->
+			</div><!-- .wbcom-welcome-support-info -->
+		</div><!-- .wbcom-welcome-content -->
+	</div><!-- .wbcom-welcome-main-wrapper -->
+</div><!-- .wbcom-tab-content -->
