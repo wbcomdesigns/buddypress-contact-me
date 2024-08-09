e4bb5f36eeca101d0d4209cc39163f675e28d0a4
Nitin Patil <pallavi@wbcomdesigns.com>
2024-08-09 18:17:28 +0530

    Manage selectize for initiate contact
    https://3.basecamp.com/5798509/buckets/37594862/card_tables/cards/7695935677

--- admin/js/buddypress-contact-me-admin.js
+++ admin/js/buddypress-contact-me-admin.js
@@ -43,7 +43,7 @@
                 }
             }
         }
-        jQuery('#bcm-multiple-user-copy-email,#bcm-who-contacted').selectize({
+        jQuery('#bcm-multiple-user-copy-email, #bcm-who-contacted, #bcm-who-contact').selectize({
             plugins: ["remove_button"],
         });
     });
